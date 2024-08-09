1cf872fe7273b2553b40de222acb766f8b02f479
jmarmagno <74318109+jmarmagno@users.noreply.github.com>
2024-08-09 09:39:17 -0300

    octavo commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa Bootstrap JS para que funcione el menú desplegable
 import './custom.css'; // Importar el archivo CSS personalizado
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 
